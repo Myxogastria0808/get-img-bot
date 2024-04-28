bc00eb2835ee9de29139bfacc38da457370f7511
Myxogastria0808 <r.rstudio.c@gmail.com>
2024-04-29 03:18:35 +0900

    Initial commit

--- dist/slashcommands/commands/utilities/ping.js
+++ dist/slashcommands/commands/utilities/ping.js
@@ -3,9 +3,23 @@ Object.defineProperty(exports, "__esModule", { value: true });
 exports.ping = void 0;
 const discord_js_1 = require("discord.js");
 const ping = {
-    data: new discord_js_1.SlashCommandBuilder().setName('ping').setDescription('Sample slash command.'),
+    data: new discord_js_1.SlashCommandBuilder()
+        .setName('upload')
+        .setDescription('画像のアップロード')
+        .addAttachmentOption((option) => option.setName('attachment').setDescription('画像のアップロード').setRequired(true)),
     async execute(interaction) {
-        interaction.reply('Pong!');
+        const proxyUrl = interaction.options.get('attachment')?.attachment?.proxyURL;
+        await interaction.reply(`${proxyUrl}`);
+        console.log(proxyUrl);
+        if (typeof proxyUrl === 'undefined')
+            return;
+        const data = await fetch(proxyUrl, {
+            method: 'GET',
+            headers: {
+                'Content-Type': 'image/png',
+            },
+        });
+        console.log(data.body);
     },
 };
 exports.ping = ping;
--- src/slashcommands/commands/utilities/ping.ts
+++ src/slashcommands/commands/utilities/ping.ts
@@ -1,9 +1,24 @@
 import { CommandInteraction, SlashCommandBuilder } from 'discord.js';
 
 const ping = {
-    data: new SlashCommandBuilder().setName('ping').setDescription('Sample slash command.'),
+    data: new SlashCommandBuilder()
+        .setName('upload')
+        .setDescription('画像のアップロード')
+        .addAttachmentOption((option) =>
+            option.setName('attachment').setDescription('画像のアップロード').setRequired(true)
+        ),
     async execute(interaction: CommandInteraction) {
-        interaction.reply('Pong!');
+        const proxyUrl = interaction.options.get('attachment')?.attachment?.proxyURL;
+        await interaction.reply(`${proxyUrl}`);
+        console.log(proxyUrl);
+        if (typeof proxyUrl === 'undefined') return;
+        const data: Response = await fetch(proxyUrl, {
+            method: 'GET',
+            headers: {
+                'Content-Type': 'image/png',
+            },
+        });
+        const binary: ReadableStream<Uint8Array> | null = data.body;
     },
 };
 
